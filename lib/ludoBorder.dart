386e3756ba416fae3fc9d4529d9423d30b7a7354
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-08-11 21:47:31 +0500

    Media Query Practice

--- lib/ludoBorder.dart
+++ lib/ludoBorder.dart
@@ -1,29 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:practiceapp/playerBox.dart';
 
-class LudoBorder extends StatelessWidget {
-  const LudoBorder({super.key});
+class Ludoborder extends StatelessWidget {
+  const Ludoborder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 600,
-          height: 600,
-          color: Colors.grey,
-          child: Stack(
-            children: [
-              Positioned(left: 0, top: 0, child: Playerbox(color: Colors.red)),
-              Positioned(
-                right: 0,
-                top: 0,
-                child: Playerbox(color: Colors.blue),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:practiceapp/ludoBorder.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LudoBorder());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Ludoborder());
   }
 }
